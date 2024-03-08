aa468f1e1fce2df1756ac55bcb70d24421fe25d2
Aaru-1895 <artiprajapati24598@gmail.com>
2024-03-08 18:05:57 +0530

    Initial commit

---
+++ src/main/java/com/web/WebSocketDemo/WebSocketDemoApplication.java
@@ -0,0 +1,14 @@
+package com.web.WebSocketDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketDemoApplication.class, args);
+		System.out.println("Websocket calling");
+	}
+
+}
